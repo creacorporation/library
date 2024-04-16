3b3aa317cff7b8e88a45f73d798e6ffd13820d6d
ccfingerling <ccfingerling@creacorp.jp>
2024-04-16 15:38:40 +0900

    svncheckをgitを使うように変更

--- svncheck.bat
+++ svncheck.bat
@@ -6,34 +6,35 @@ echo #define %1_ENABLE_WEBVIEW2
 :libcheck1
 
 rem ----- SVNリビジョンチェック -----
-svn help > nul
+git help > nul
 if errorlevel 9009 goto errend
 
-svn info . | findstr "Revision:" > tmp_svnversion.txt
+git show --format='%%h' --no-patch > tmp_svnversion.txt
 for /F "DELIMS=" %%t in ( tmp_svnversion.txt ) do set SVNREVISION=%%t
-set SVNREVISION=%SVNREVISION:~10%
+set SVNREVISION=%SVNREVISION:~1,7%
 set SVNREVISIONSTR=%SVNREVISION%
-set SVNMODIFIED=0
+
+git show --format='%%H' --no-patch > tmp_svnversion.txt
+for /F "DELIMS=" %%t in ( tmp_svnversion.txt ) do set SVNREVISIONLONGSTR=%%t
+set SVNREVISIONLONGSTR=%SVNREVISIONLONGSTR:~1,40%
+
 del tmp_svnversion.txt
 
-svn st . | findstr "^[MA!]" > nul
-if errorlevel 1 goto end
-set SVNREVISIONSTR=%SVNREVISION%-MOD
-set SVNMODIFIED=1
-
 :end
-echo #define %1_REVISION_NUMBER %SVNREVISION%
+echo #define %1_REVISION_NUMBER 0x%SVNREVISION%u
 echo #define %1_REVISION_STRING "%SVNREVISIONSTR%"
-echo #define %1_REVISION_MODIFIED %SVNMODIFIED%
+echo #define %1_REVISION_STRING_LONG "%SVNREVISIONLONGSTR%"
+echo #define %1_REVISION_MODIFIED 0
 echo #define %1_REVISION_EXPORTED 0
 set SVNREVISION=
-set REVISION_STRING=
-set REVISION_MODIFIED=
+set SVNREVISIONSTR=
+set SVNREVISIONLONGSTR=
 exit /B 0
 
 :errend
 echo #define %1_REVISION_NUMBER 0
 echo #define %1_REVISION_STRING "EXPORTED"
+echo #define %1_REVISION_STRING_LONG "EXPORTED"
 echo #define %1_REVISION_MODIFIED 0
 echo #define %1_REVISION_EXPORTED 1
 exit /B 0
