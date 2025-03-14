c5245b53a62d4df218e3542d640d1547d7ed5c39
fingerling <ccfingerling@creacorp.jp>
2025-03-14 15:48:05 +0900

    インクルードガードがなかった

--- Type/mProperty.h
+++ Type/mProperty.h
@@ -1,3 +1,16 @@
+//----------------------------------------------------------------------------
+// プロパティ値
+// Copyright (C) 2025- Crea Inc. All rights reserved.
+// This program is released under the MIT License. 
+// see http://opensource.org/licenses/mit-license.php
+// 著作権表示やライセンスの改変は禁止されています。
+// このソースコードに関して、上記ライセンス以外の契約等は一切存在しません。
+// (何らかの契約がある場合でも、本ソースコードはその対象外となります)
+//----------------------------------------------------------------------------
+
+#ifndef MPROPERTY_H_INCLUDED
+#define MPROPERTY_H_INCLUDED
+
 #include "mStandard.h"
 
 template< class T , class F = T >
@@ -78,3 +91,5 @@ protected:
 	bool MyOn;
 
 };
+
+#endif
